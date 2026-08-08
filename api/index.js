e303b1fabcc183f2319f3d883eb7c2c55db63225
7xsoam-ops <7xsoam@gmail.com>
2026-08-09 00:53:23 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,1 @@
-<meta property="C" content="https://7xsoam.vercel.app/api/admin">
-<meta property="og:title" content="Omega Star Panel 💖">
-<meta property="og:description" content="Secure & Official Tool. Click to Download.">
-<meta property="og:image" content="https://images.emojiterra.com/google/android-10/512px/2764.png">
-<meta property="og:url" content="https://7xsoam.vercel.app/">
+
